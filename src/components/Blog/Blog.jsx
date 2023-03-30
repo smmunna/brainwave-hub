3763aa083cf17a4d64481a4b7e833daecd99ccd1
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-30 22:08:04 +0600

    Blog Component and Sidebar created

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Blog.css';
+const Blog = () => {
+    return (
+        <div>
+            <h3>Blog</h3>
+        </div>
+    );
+}
+
+export default Blog;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Sidebar.css';
+const Sidebar = () => {
+    return (
+        <div>
+            <h3>SideBar</h3>
+        </div>
+    );
+}
+
+export default Sidebar;
