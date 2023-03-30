9b30966f56f97f040b5404c8ad75ce14c225ffe8
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-30 23:16:36 +0600

    Update Blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleBlog from '../SingleBlog/SingleBlog';
 import Sidebar from '../Sidebar/Sidebar';
 import './Blog.css';
 const Blog = () => {
+    const[blogs,setBlogs]=useState([]);
+    useEffect(()=>{
+        fetch('brainwave.json')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div>
             <div className="blog flex flex-col-reverse lg:flex-row">
