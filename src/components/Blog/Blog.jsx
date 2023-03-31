448869e7ea39ebb5bc94a428cfa4c6c0600de7b9
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-31 09:03:04 +0600

    Update Blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,7 +5,7 @@ import './Blog.css';
 const Blog = () => {
     const [blogs, setBlogs] = useState([]);
     const [readTime, setreadTime] = useState("");
-    
+
     useEffect(() => {
         fetch('brainwave.json')
             .then(res => res.json())
@@ -13,14 +13,14 @@ const Blog = () => {
     }, [])
 
     // Onclick button added from SingleBox.jsx;
-    const handleMarkasRead = (read_time)=>{
+    const handleMarkasRead = (read_time) => {
         const previousReadTime = JSON.parse(localStorage.getItem('read-time'))
         if (previousReadTime) {
             const newReadTime = previousReadTime + read_time;
-            localStorage.setItem('read-time',newReadTime);
+            localStorage.setItem('read-time', newReadTime);
             setreadTime(newReadTime);
         } else {
-            localStorage.setItem('read-time',read_time);
+            localStorage.setItem('read-time', read_time);
             setreadTime(read_time);
         }
     }
@@ -28,17 +28,17 @@ const Blog = () => {
         <div>
             <div className="blog lg:px-16">
                 <div className="blog-section lg:px-2">
-                {
-                    blogs.map(blog=><SingleBlog 
-                    key={blog.id}
-                    blog={blog}
-                    // passing the function to SingleBlog.jsx;
-                    handleMarkasRead={handleMarkasRead}
-                     />)
-                }
+                    {
+                        blogs.map(blog => <SingleBlog
+                            key={blog.id}
+                            blog={blog}
+                            // passing the function to SingleBlog.jsx;
+                            handleMarkasRead={handleMarkasRead}
+                        />)
+                    }
                 </div>
                 <div className="sidebar m-2 lg:mt-6">
-                    <Sidebar readTime={readTime}/>
+                    <Sidebar readTime={readTime} />
                 </div>
             </div>
         </div>
