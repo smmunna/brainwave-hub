00ff885a8d8079d41bfadc63727a3d45a698b344
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-31 09:18:04 +0600

    update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,6 +11,13 @@ const Sidebar = ({ readTime }) => {
             <div className="spent-time text-center">
                 <h2>Spent time on read: {getreadTime ? getreadTime : 0} min</h2>
             </div>
+            {/* BookedMarked Blogs */}
+            <div className='bookmarked-blog mt-5 p-7'>
+                <h2>Bookmarked Blogs: 8</h2>
+                <div className='bookmarked-text'>
+                    Master MicroSoft Power Platfor and Become and in Demand !
+                </div>
+            </div>
         </div>
     );
 }
