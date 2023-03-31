fd162a5a2215ff329bfee87060a0cf2abfa08375
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-31 11:33:16 +0600

    Update SingleBlog.jsx

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -26,7 +26,11 @@ const SingleBlog = ({ blog, handleMarkasRead, handleBookmarkedBlog }) => {
                     <div className='flex gap-3'>
                         <div>0{read_time} Minutes to read</div>
                         <div className='bookmark-icon'>
-                            <FontAwesomeIcon onClick={() => handleBookmarkedBlog(blog_title)} className='cursor-pointer' style={bookmarkColor} title='Add to Bookmark' icon={faBookmark} />
+                            <span onClick={() => handleBookmarkedBlog(blog_title)} className='cursor-pointer' style={bookmarkColor} title='Add to Bookmark'>
+                                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                </svg>
+                            </span>
                         </div>
                     </div>
                 </div>
