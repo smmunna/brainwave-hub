fb1a28cb6027ff2541d1aa11460d89b5be3f1b59
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-31 09:03:21 +0600

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './Sidebar.css';
-const Sidebar = ({readTime}) => {
-    const[getreadTime,setgetReadTime]=useState(readTime);
-   useEffect(()=>{
-    const getReadTime = JSON.parse(localStorage.getItem('read-time'));
-    setgetReadTime(getReadTime)
-   },[readTime])
+const Sidebar = ({ readTime }) => {
+    const [getreadTime, setgetReadTime] = useState(readTime);
+    useEffect(() => {
+        const getReadTime = JSON.parse(localStorage.getItem('read-time'));
+        setgetReadTime(getReadTime)
+    }, [readTime])
     return (
         <div>
             <div className="spent-time text-center">
-                <h2>Spent time on read: {getreadTime?getreadTime:0} min</h2>
+                <h2>Spent time on read: {getreadTime ? getreadTime : 0} min</h2>
             </div>
         </div>
     );
