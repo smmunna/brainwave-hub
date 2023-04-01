34da0ba51d26dd36183c4a3cf7b63768383ed8c9
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-01 13:23:12 +0600

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -27,11 +27,11 @@ const Sidebar = ({ readTime, bookmarked }) => {
             <div className='bookmarked-blog mt-5 p-7'>
                 <h2>Bookmarked Blogs: {counter} </h2>
                 {
-                    bookmarked.map(bookmark => <>
-                        <div className='bookmarked-text' key={bookmark}>
+                    bookmarked.map((bookmark, index) => (
+                        <div className='bookmarked-text' key={index}>
                             {bookmark}
                         </div>
-                    </>)
+                    ))
                 }
             </div>
 
