13b12423753e58ff91219b7418cbd4ef002fd6dd
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-01 14:59:42 +0600

    Update SingleBlog.jsx

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -12,7 +12,7 @@ const SingleBlog = ({ blog, handleMarkasRead, handleBookmarkedBlog }) => {
     const handleBookmarkColor = () => {
         if (!isBookmarkColor) {
             setIsBookMarkColor(true);
-          }
+        }
     }
 
     return (
@@ -35,8 +35,8 @@ const SingleBlog = ({ blog, handleMarkasRead, handleBookmarkedBlog }) => {
                         <div>0{read_time} Minutes to read</div>
                         <div className='bookmark-icon'>
                             <span>
-                                <button onClick={() => handleBookmarkedBlog(blog_title)}  className='cursor-pointer' style={bookmarkColor} title='Add to Bookmark'>
-                                    <svg onClick={() => handleBookmarkColor()}  xmlns="http://www.w3.org/2000/svg" fill={isBookmarkColor ? 'black' : 'none'} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <button onClick={() => handleBookmarkedBlog(blog_title)} className='cursor-pointer' style={bookmarkColor} title='Add to Bookmark'>
+                                    <svg onClick={() => handleBookmarkColor()} xmlns="http://www.w3.org/2000/svg" fill={isBookmarkColor ? 'black' : 'none'} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                                     </svg>
                                 </button>
