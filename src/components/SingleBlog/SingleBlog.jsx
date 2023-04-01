b199a1609545d7e28997f37bc26dbcd8e076e3d2
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-04-01 13:35:07 +0600

    Update SingleBlog.jsx

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './SingleBlog.css';
 
 const bookmarkColor = {
-    color: "red"
+    color: "black"
 }
 const SingleBlog = ({ blog, handleMarkasRead, handleBookmarkedBlog }) => {
     const { id, author_name, blog_title, author_img, blog_cover, read_time, published_date } = blog;
@@ -11,6 +11,7 @@ const SingleBlog = ({ blog, handleMarkasRead, handleBookmarkedBlog }) => {
     const[isBookmarkColor,setIsBookMarkColor]=useState(false);
     const handleBookmarkColor = ()=>{
         setIsBookMarkColor(!isBookmarkColor);
+        
     }
     
     return (
@@ -33,7 +34,7 @@ const SingleBlog = ({ blog, handleMarkasRead, handleBookmarkedBlog }) => {
                         <div>0{read_time} Minutes to read</div>
                         <div className='bookmark-icon'>
                             <span onClick={() => handleBookmarkedBlog(blog_title)} className='cursor-pointer' style={bookmarkColor} title='Add to Bookmark'>
-                                <svg onClick={()=>handleBookmarkColor()}  xmlns="http://www.w3.org/2000/svg" fill={isBookmarkColor?'red' : 'none'} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <svg onClick={()=>handleBookmarkColor()}  xmlns="http://www.w3.org/2000/svg" fill={isBookmarkColor?'black' : 'none'} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                                 </svg>
                             </span>
